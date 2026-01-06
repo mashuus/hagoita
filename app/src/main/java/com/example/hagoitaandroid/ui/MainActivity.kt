25c06b4ab191c11bdc00fe22ba4dbb85e92f2600
Ryu-375 <uchida.ryusei.xyz@gmail.com>
2026-01-06 18:58:26 +0900

    画像の表示と画面遷移の一時的なコミット

--- app/src/main/java/com/example/hagoitaandroid/ui/MainActivity.kt
+++ app/src/main/java/com/example/hagoitaandroid/ui/MainActivity.kt
@@ -7,10 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.hagoitaandroid.ui.theme.HagoitaandroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HagoitaandroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HagoitaApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +30,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun HagoitaApp(modifier: Modifier = Modifier) {
+    // NavController: 画面遷移を命令するコントローラー
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HagoitaandroidTheme {
-        Greeting("Android")
+    // ViewModelのインスタンスをここで一度だけ作成し、各画面で共有する
+    val gameViewModel: GameViewModel = viewModel()
+
+    // NavHost: 現在表示すべき画面を描画する場所
+    NavHost(
+        navController = navController,
+        startDestination = "start",
+        modifier = modifier
+    ) {
+        // composable("ルート名") { 表示する画面のUI } という形で画面を定義する
+        composable("start") {
+            // スタート画面を表示 (UIの定義はGameScreen.ktにある)
+            GameStartScreen(
+                // スタート画面がクリックされたら、以下の処理を実行する
+                onStartClick = {
+                    gameViewModel.startGame() // ViewModelのゲーム開始関数を呼ぶ
+                    navController.navigate("play") // "play"画面に遷移する
+                }
+            )
+        }
+
+        composable("play") {
+            // ゲームプレイ画面を表示 (UIの定義はGameScreen.ktにある)
+            // HagoitaAppで作成したViewModelを渡す
+            GamePlayScreen(gameViewModel = gameViewModel)
+        }
     }
 }
